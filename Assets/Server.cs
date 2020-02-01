f27d4b45ab77f4e9477775850f880ec3ebc80af1
Exifience <henrypan1999@gmail.com>
2020-02-01 21:33:52 +1100

    server client prelim finish

--- Assets/Server.cs
+++ Assets/Server.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Net.Sockets;
 using System;
 using UnityEngine;
+using System.Net;
+using System.IO;
 
-public class Server : MonoBehaviour
-{
+public class Server : MonoBehaviour {
     public int port = 2020;
 
     private List<ServerClient> clients;
@@ -27,32 +28,94 @@ public class Server : MonoBehaviour
 			serverStarted = true;
 		}
 		catch(Exception e) {
-			debug.Log("Socket error: " + e.Message);
+			Debug.Log("Socket error: " + e.Message);
 		}
     }
 
-    private void Update(
+    private void Update() {
+    	if (!serverStarted) {
+    		return;
+    	}
 
+    	foreach (ServerClient c in clients) {
+    		//Is the client still connected
+    		if(!IsConnected(c.tcp)) {
+    			c.tcp.Close();
+    			disconnectList.Add(c);
+    			continue;
+    		} else {
+    			NetworkStream s  = c.tcp.GetStream();
+    			if(s.DataAvailable) {
+    				StreamReader reader = new StreamReader(s, true);
+    				string data = reader.ReadLine();
+
+    				if(data != null)
+    					OnIncomingData(c,data);
+    			}
+    		}
+    	}
+    	for(int i = 0; i < disconnectList - 1; i++) {
+    		//Tell player someone disconnects
+
+    		clients.Remove(disconnectList[i]);
+    		disconnectList.RemoveAt(i);
+    	}
     }
 
     private void StartListening() {
 		server.BeginAcceptTcpClient(AcceptTcpClient, server);
     }
 
-    private void AcceptTcpClient(IsAsyncResult ar) {
+    private void AcceptTcpClient(IAsyncResult ar) {
 		TcpListener listner = (TcpListener)ar.AsyncState;
 
 		Server sc = new ServerClient(listener.EndAcceptTcpClient(ar));
-		clients.add(sc);
+		clients.Add(sc);
 
 		StartListening();
 
-		Debug.log("Somebody has Connected!");
+		Debug.Log("Somebody has Connected!");
     }
+
+    private bool IsConnected(TcpClient c) {
+    	try {
+			if(c != null && c.Client != null && c.Client.Connected) {
+				if(c.Client.Poll(0, SelectMode.SelectRead))
+					return !(c.Client.Receive(new byte[1], SocketFlags.Peek == 0));
+
+				return true;
+			}
+			else
+				return false;
+    	}
+    	catch {
+    		return false;
+    	}
+    }
+
+    //Server Send
+    private void Broadcast(string data, List<ServerClient> cl) {
+    	foreach(ServerClient sc in cl) {
+    		try{
+    			StreamWriter writer = new StreamWriter(sc.tcp.GetStream());
+    			writer.WriteLine(data);
+    			writer.Flush();
+    		}
+    		catch(Exception e) {
+    			Debug.Log("Write error: " + e.Message);
+    		}
+    	}
+    }
+
+    //Server Read
+    private void OnIncomingData(ServerClient c, string data) {
+    	Debug.Log(data);
+    }
+
+
 }
 
-public class ServerClient
-{
+public class ServerClient {
 	public string clientName;
 	public TcpClient tcp;
 
